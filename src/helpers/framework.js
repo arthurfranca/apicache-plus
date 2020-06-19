93d6d0c910a8bb48f04dfe32c42f3b9f83fd2b61
arthurfranca <arthur.a.franca@gmail.com>
2020-06-19 11:22:42 -0300

    Refactor isKoa helper

--- src/helpers/framework.js
+++ src/helpers/framework.js
@@ -1,5 +1,7 @@
 function isKoa(ctx) {
-  return ctx && typeof ctx === 'object' && ctx.request && ctx.response && ctx.req && ctx.res
+  return Boolean(
+    ctx && typeof ctx === 'object' && ctx.request && ctx.response && ctx.req && ctx.res
+  )
 }
 
 module.exports = {
